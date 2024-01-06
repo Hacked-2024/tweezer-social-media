81d0d621857e181090824989effc21327b5fa6e2
Mihiri Kamiss <76460057+Jaymerist@users.noreply.github.com>
2024-01-06 12:10:23 -0700

    Added header template

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $title?></title>
+</head>
+<body>
+    <!-- Create a navbar within the header tags using bootstrap
+    Requirements:
+        - Change the color to a custom (not bs library default) color
+        - Add the logo on the left side of the navbar brand ("Tweezer")
+        - add three icons to the right side of the navbar: a human profile icon, a home icon and a settings icon. The icons should be links (<a> tags)
+    -->
+    <header>
+
+    </header>
